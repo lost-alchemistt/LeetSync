f670fddbe02186558ea1f9bd978548e858dc5eb5
lost-alchemistt <Chetan.v.kalsariya@gmail.com>
2026-05-16 11:52:19 +0530

    Test

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 // config.example.js
 export const secrets = {
   GITHUB_TOKEN: "YOUR_TOKEN_HERE",
-  REPO_OWNER: "lost-alchemistt",
-  REPO_NAME: "Leetcode-Submissions"
+  REPO_OWNER: "repo owner",
+  REPO_NAME: "repo name"
 };
